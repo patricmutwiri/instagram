8170ce88f4c938af43fda2669614b64c74644384
Patrick <patwiri@gmail.com>
2016-08-02 10:48:05 +0300

    version

--- ig.js
+++ ig.js
@@ -3,7 +3,7 @@
   var newscript = document.createElement('script');
      newscript.type = 'text/javascript';
      newscript.async = true;
-     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js';
   (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
 })();
 
